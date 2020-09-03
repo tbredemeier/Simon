332678633c65bd69160fbead6b18eee42fd5f7d1
Tom Bredemeier <tbredemeier@barrington220.org>
2020-09-02 20:28:29 -0500

    Added touch recognition

--- Simon/ViewController.swift
+++ Simon/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet var colorDisplays: [UIView]!
+    @IBOutlet weak var colorsFrame: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        flashColor(number: 0)
+        flashColor(number: 1)
+        flashColor(number: 2)
+        flashColor(number: 3)
     }
 
+    func flashColor(number: Int) {
+        UIView.transition(with: colorDisplays[number], duration: 0.5, options: .transitionCrossDissolve, animations: {
+            self.colorDisplays[number].alpha = 1.0
+        }) { (true) in
+            UIView.transition(with: self.colorDisplays[number], duration: 0.5, options: .transitionCrossDissolve, animations: {
+                self.colorDisplays[number].alpha = 0.4
+            }, completion: nil)
+        }
+    }
+
+    @IBAction func onStartButtonTapped(_ sender: Any) {
+    }
+
+    @IBAction func onColorTapped(_ sender: UITapGestureRecognizer) {
+        for index in 0..<colorDisplays.count {
+             if colorDisplays[index].frame.contains(sender.location(in: colorsFrame)) {
+                 flashColor(number: index)
+             }
+         }
+    }
 
 }
 
